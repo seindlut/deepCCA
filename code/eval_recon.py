df117c319e01fa7bf4fd4a4c18105c1fb9a675dc
Maha ELBAYAD <maha.elbayad.intern@3ds.com>
2016-05-17 11:49:34 +0200

    up

--- code/eval_recon.py
+++ code/eval_recon.py
@@ -21,15 +21,15 @@ def da_recon(da, x, corruption_level):
     y = da.get_hidden_values(tilde_x)
     z = da.get_reconstructed_input(y)
     return z
-#
-# test_da = theano.function(
-#     [index],
-#     da_recon(da=da,x=x, corruption_level = .0),
-#     givens={
-#         x: test_set_x[index]
-#     }
-# )
 
-z = da_recon(da,test_set_x[0], .3)
+test_da = theano.function(
+    [index],
+    da_recon(da=da,x=x, corruption_level = .0),
+    givens={
+        x: test_set_x[index]
+    }
+)
+
+z  test_da(0)
 print 'Input: ', test_set_x[0]
 print 'Recon: ',z
